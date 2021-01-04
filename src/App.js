c09011956e0d2f723c1e8dd412c67927efc58c0a
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2021-01-04 17:11:44 -0500

    create useeEffect, use State for api handling, async funct to handle API, create recipe.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from "react";
-import logo from './logo.svg';
-
+import React, { useEffect, useState } from "react";
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
 
   const APP_ID = 'b5b8a081';
   const APP_KEY = '68c5c9c18b4c38368e3a8ad7169c6b5c';
-  const ExampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`)
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
+
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   )
 }
