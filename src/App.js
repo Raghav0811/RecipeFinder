1b332023a6ac20db1a5fba298c1b548929c56427
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2021-01-04 13:38:30 -0500

    add API id, key and example request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from "react";
 import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = 'b5b8a081';
+  const APP_KEY = '68c5c9c18b4c38368e3a8ad7169c6b5c';
+  const ExampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
+
   return (
     <div className="App">
       <h1>Hello React</h1>
     </div>
-  );
+  )
 }
+
+
 
 export default App;
