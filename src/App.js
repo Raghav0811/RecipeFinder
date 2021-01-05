c340a1636a6879236ced8870affcd0b3c5209a86
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2021-01-05 13:58:03 -0500

    style recipe component and add math ceiling to calorie

--- src/App.js
+++ src/App.js
@@ -8,34 +8,49 @@ const App = () => {
   const APP_KEY = '68c5c9c18b4c38368e3a8ad7169c6b5c';
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState([]);
+  const [query, setQuery] = useState(['chicken']);
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`)
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`)
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
   }
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">
           Search
         </button>
       </form>
-      {recipes.map(recipe => (
-        <Recipe
-          key={recipe.recipe.label}
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image} />
-      ))}
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={`${Math.ceil(recipe.recipe.calories)} Kcal`}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients} />
+        ))}
+      </div>
     </div>
   )
 }
